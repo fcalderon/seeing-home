495ef3a65cf0b8bda0f8f102155eba273d2f0b26
Francisco Calderon <fjavier5152@gmail.com>
2019-06-02 19:12:40 -0400

    SH - Upgrade Angular to 8.0

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,14 +4,12 @@ import { RouterModule } from '@angular/router';
 import { AuthServicesModule } from './auth-services.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
-import {
-  MatInputModule,
-  MatButtonModule,
-  MatSelectModule,
-  MatRadioModule,
-  MatCardModule,
-  MatCheckboxModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { LayoutModule } from '@angular/cdk/layout';
 import { UserNavigationComponent } from './user-navigation/user-navigation.component';
 
